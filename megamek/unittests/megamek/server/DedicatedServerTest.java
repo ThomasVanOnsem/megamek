46eab5b62b42da3cefcc215b5e85dfc4324b4189
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2021-05-22 11:58:48 +0200

    DedicatedServer tests

---
+++ megamek/unittests/megamek/server/DedicatedServerTest.java
@@ -0,0 +1,34 @@
+package megamek.server;
+
+import junit.framework.TestCase;
+import megamek.common.util.AbstractCommandLineParser;
+import org.junit.Test;
+
+public class DedicatedServerTest {
+
+    @Test
+    public void testCommandLineArgsFull() throws AbstractCommandLineParser.ParseException {
+        String[] args = {"-password", "pass", "-port", "1234", "-competitive", "true"};
+        DedicatedServer.CommandLineParser cp = new DedicatedServer.CommandLineParser(args);
+        cp.parse();
+        TestCase.assertEquals("pass", cp.getPassword());
+        TestCase.assertEquals(1234, cp.getPort());
+        TestCase.assertTrue(cp.getCompetitive());
+    }
+
+    @Test
+    public void testCommandLineArgsPart() throws AbstractCommandLineParser.ParseException {
+        String[] args = {"-password", "pass", "-port", "1234"};
+        DedicatedServer.CommandLineParser cp = new DedicatedServer.CommandLineParser(args);
+        cp.parse();
+        TestCase.assertEquals("pass", cp.getPassword());
+        TestCase.assertEquals(1234, cp.getPort());
+        TestCase.assertFalse(cp.getCompetitive());
+    }
+
+    @Test
+    public void testDedicatedServer() {
+        DedicatedServer.start(new String[]{"-password", "pass", "-port", "1234"});
+    }
+
+}
