131cf3c27e2a94bab08a26999e2df2ef1f52e371
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2021-05-22 14:20:34 +0200

    Client test handling of ServerGreeting packet

--- megamek/unittests/megamek/client/ClientTest.java
+++ megamek/unittests/megamek/client/ClientTest.java
@@ -1,6 +1,7 @@
 package megamek.client;
 
 import junit.framework.TestCase;
+import megamek.common.net.Packet;
 import org.junit.Test;
 
 public class ClientTest {
@@ -17,4 +18,21 @@ public class ClientTest {
         TestCase.assertEquals(port, c.getPort());
         TestCase.assertEquals(password, c.getPassword());
     }
+
+    @Test
+    public void testServerGreetingPacket() {
+        String name = "TestClient";
+        String host = "localhost";
+        int port = 3456;
+        String password = "pass";
+        Client c = new Client(name, host, port, password);
+        c.setTesting(true);
+        c.handlePacket(new Packet(Packet.COMMAND_SERVER_GREETING));
+        TestCase.assertEquals(2, c.getTestingOutPackets().size());
+        Packet result = c.getTestingOutPackets().get(0);
+        TestCase.assertEquals(Packet.COMMAND_CLIENT_NAME, result.getCommand());
+        TestCase.assertEquals(name, result.getObject(0));
+        TestCase.assertEquals(password, result.getObject(1));
+    }
+
 }
