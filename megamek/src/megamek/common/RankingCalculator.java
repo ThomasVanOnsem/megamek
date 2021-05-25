1f1948e195aed8fcc6b169d026473649b6c1caca
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2021-05-25 12:33:49 +0200

    Add comments to RankingCalculator

--- megamek/src/megamek/common/RankingCalculator.java
+++ megamek/src/megamek/common/RankingCalculator.java
@@ -3,6 +3,9 @@ package megamek.common;
 import megamek.common.event.GameEndEvent;
 import megamek.common.event.GameListenerAdapter;
 
+/**
+ * Static methods for updating the rankings of players in a competitive context
+ */
 public class RankingCalculator {
 
     private RankingCalculator() {}
@@ -12,14 +15,14 @@ public class RankingCalculator {
      * @param e
      */
     public static void updateRankings(GameEndEvent e) {
-
+        // To be implemented
     }
 
     /**
      * Notify the ranking system the client left the game without it ending.
      */
     public static void notifyClientDisconnect(){
-
+        // To be implemented
     }
 
     public static class RankingGameListener extends GameListenerAdapter {
